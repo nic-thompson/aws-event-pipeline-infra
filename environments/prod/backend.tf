49c11237baa49fa90f437d5efc2b18a153c9528a
Nicolas Thompson <nicolasgthompson@gmail.com>
2026-04-24 20:22:56 +0100

    feat(terraform): provision production pipeline environment

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "signalforge-prod-terraform-state"
-    key            = "pipeline-infra/terraform.tfstate"
+    bucket         = "signalforge-terraform-state-081277286841-eu-west-2"
+    key            = "environments/prod/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "signalforge-prod-terraform-locks"
+    dynamodb_table = "signalforge-terraform-locks"
     encrypt        = true
   }
 }
